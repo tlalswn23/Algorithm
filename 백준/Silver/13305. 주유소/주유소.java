fb0a9fa08bffae6c8f12d7493db8162bf9e24119
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-01-08 11:34:21 +0900

    [Silver III] Title: 주유소, Time: 404 ms, Memory: 60872 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,34 +1,38 @@
 
 import java.util.*;
 import java.io.*;
+import java.math.BigDecimal;
 
 public class Main {
-	static int N, min, total;
-	static int[] d, w; 
+	static int N;
+	static BigDecimal min, total;
+	static BigDecimal[] d, w; 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		d = new int[N-1];
-		w = new int[N-1];
+		d = new BigDecimal[N-1];
+		w = new BigDecimal[N-1];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N-1; i++) {
-			d[i] = Integer.parseInt(st.nextToken());
+			d[i] = new BigDecimal(st.nextToken());
 		}
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N-1; i++) {
-			w[i] = Integer.parseInt(st.nextToken());
+			w[i] = new BigDecimal(st.nextToken());
 		}
 		
 		
-		min = Integer.MAX_VALUE; // 초기화 
+		min = new BigDecimal("1000000000"); // 초기화 
+		total = BigDecimal.ZERO;
 		for(int i = 0; i < N-1; i++) {
-			if(min >= w[i]) { // min 값과 같거나 작으면 min 값을 갱신
+			
+			if(min.compareTo(w[i]) >= 0) { // min 값과 같거나 작으면 min 값을 갱신
 				min = w[i];
 			}
 			
-			total += d[i]*min;
+			total = total.add(d[i].multiply(min));
 		}
 		
 		System.out.println(total);
