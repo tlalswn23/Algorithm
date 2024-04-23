08c3156c5f2461156cea045dd4716733efebe386
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-04-23 20:40:04 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.45 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            arr[i] = (arr[i-1] + arr[i-2]) % 1000000007;
+        }
+        
+        return arr[n];
+    }
+}
