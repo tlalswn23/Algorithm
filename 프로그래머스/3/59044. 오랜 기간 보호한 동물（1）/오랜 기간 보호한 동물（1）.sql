e72e208547df9e709ac9dea260584d6b55567412
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-05-10 21:32:19 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
 SELECT a.name, a.datetime
-from animal_ins a
-left join animal_outs b on a.animal_id = b.animal_id
-order by datediff(b.datetime, a.datetime), a.datetime
-limit 3
+from ANIMAL_INS a left join ANIMAL_OUTS b on a.ANIMAL_ID = b.ANIMAL_ID
+where b.datetime is null
+order by a.datetime limit 3
