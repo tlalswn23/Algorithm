9c674167be5ec2a6112f26d51d9aa37df0264d98
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-10-16 00:11:24 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT a.animal_id, a.name
-from animal_ins a
-left join animal_outs b on a.animal_id = b.animal_id
+SELECT a.animal_id as animal_id, a.name as name
+from animal_ins as a
+left join animal_outs as b on a.animal_id = b.animal_id
 where a.datetime > b.datetime
 order by a.datetime
