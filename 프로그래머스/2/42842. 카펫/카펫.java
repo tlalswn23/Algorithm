5c1c92abb2aef44e6cda16586b6aff1afc71c5d2
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-10-15 21:53:39 +0900

    [level 2] Title: 카펫, Time: 11.50 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,39 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static int total, b, y;
+    static int B, Y;
     
     public int[] solution(int brown, int yellow) {
-        total = brown + yellow;
-        b = brown;
-        y = yellow;
-        int start = total/2;
+        int[] answer = new int[2];
+        B = brown;
+        Y = yellow;
         
-        int w = 0, h = 0;
-        for(int i = start; i > 0; i--){
-            if(total%i != 0){
-                continue;
-            }
-            
-            w = i;
-            h = total/i;
-            
-            if(findCarpet(w, h)){
-                break;
+        for(int i = 2; i <= (brown/2); i++){
+            for(int j = 1; j <= i; j++){
+                if(check(i, j)){
+                    answer[0] = i;
+                    answer[1] = j;
+                    
+                    return answer;
+                }
             }
         }
         
-        return new int[] {w, h};
+        return answer;
     }
     
-    static boolean findCarpet(int w, int h){
-        int border = w*2 + h*2 - 4;
-        int inner = total - border;
+    static boolean check(int r, int c){
+        // r, c일 때 수가 각 개수가 맞는지 확인
+        int cntB = (r*2) + ((c-2)*2);
+        int cntY = (r-2) * (c-2);
         
-        if(border == b && inner == y){
+        if(cntB == B && cntY == Y){
             return true;
         }
         
         return false;
     }
 }
+
+// 가로의 최대 길이 : (타일 수) / 4
+// 테두리 : (가로x2) + ((세로-2)x2)
+// 가운데 : (가로-2) x (세로-2)
