2e46329782defd14904b27cfc634201106c60ed0
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-10-28 17:24:50 +0900

    [level 2] Title: 더 맵게, Time: 1565.87 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,27 +4,31 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Integer> dishes = new PriorityQueue<>();
+        
         for(int i = 0; i < scoville.length; i++){
-            queue.add(scoville[i]);
+            dishes.add(scoville[i]);
         }
         
-        while(true){
-            int soc = queue.poll();
-            if(soc >= K){
+        while(dishes.size() > 1){
+            int d1 = dishes.poll();
+            int d2 = dishes.poll();
+            int nd = d1 + (d2 * 2);
+            
+            if(d1 >= K){ // 가장 작은 값이 K보다 크면 모든 값이 K이상이므로 멈춤
                 break;
             }
             
-            if(queue.isEmpty()){
-                answer = -1;
-                break;
-            }
-            
-            int soc2 = queue.poll();
-            queue.add(soc+(soc2*2));
+            dishes.add(nd);
             answer++;
         }
         
+        if(!dishes.isEmpty()){
+            int last = dishes.poll();
+            if(last < K){
+                return -1;
+            }
+        }
         return answer;
     }
 }
